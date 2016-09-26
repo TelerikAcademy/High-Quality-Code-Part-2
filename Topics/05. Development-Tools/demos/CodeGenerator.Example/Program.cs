2182edc4d208a8228fc9b3e2de50531ce761ec76
InKolev <in.kolev.b@gmail.com>
2016-09-26 17:09:39 +0300

    Added simple demo for Code Generators

---
+++ Topics/05. Development-Tools/demos/CodeGenerator.Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new OnlineJudgeSystemEntities();
+            var contests = db.Contests
+                .OrderBy(x => x.StartTime)
+                .ToList();
+
+            foreach (var contest in contests)
+            {
+                Console.WriteLine($"{contest.Name} - {contest.StartTime}");
+            }
+        }
+    }
+}
